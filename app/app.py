4ae34cc3a8a0a3b679513954ef48eeac32d52e1a
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-11 13:15:15 +0100

    GET - Todo route with dummy data

--- app/app.py
+++ app/app.py
@@ -3,6 +3,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/", tags=['ROOT'])
+@app.get('/', tags=['ROOT'])
 async def root() -> dict:
-    return {"Ping": "Pong"}
+    return {'Ping': 'Pong'}
+
+
+@app.get('/todo', tags=['todos'])
+async def get_todo() -> dict:
+    return {'data': todos}
+
+
+todos = [
+    {
+        'id': "1",
+        'Activity': "Jogging for 2 hours at 7:00 AM."
+    },
+    {
+        'id': "2",
+        'Activity': "Writing 3 pages of my new book at 2:00 PM."
+    }
+]
