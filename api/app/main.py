913fca3adf12b8e209c64ac79fa0358fc9268bb4
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-24 00:30:44 +0100

    Commented out POST users, since auth will handle, general formatting

--- api/app/main.py
+++ api/app/main.py
@@ -5,8 +5,12 @@ from typing import Annotated
 from api.app import models
 from api.app.db.database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
+from auth import auth
 
 app = FastAPI()
+
+app.include_router(auth.router)
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,16 +26,23 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.post("/users/", status_code=status.HTTP_201_CREATED, tags=['users'])
-async def create_user(user: models.UserBase, db: db_dependency):
-    db_user = models.User(**user.dict())
-    db.add(db_user)
-    db.commit()
+@app.get('/', tags=['root'])
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    return {'Ping=': 'Pong', 'Hello': 'World'}
+
+
+# @app.post("/users/", status_code=status.HTTP_201_CREATED, tags=['users'])
+# async def create_user(user: models.UserBase, db: db_dependency):
+#     db_user = models.Users(**user.model_dump())
+#     db.add(db_user)
+#     db.commit()
 
 
 @app.get('/users/{user_id}', status_code=status.HTTP_200_OK, tags=['users'])
 async def read_get(user_id: int, db: db_dependency):
-    user = db.query(models.User).filter(user_id == models.User.id).first()
+    user = db.query(models.Users).filter(user_id == models.Users.id).first()
     if user is None:
         raise HTTPException(status_code=404, detail='User not found')
     return user
@@ -39,7 +50,7 @@ async def read_get(user_id: int, db: db_dependency):
 
 @app.post("/posts/", status_code=status.HTTP_201_CREATED, tags=['posts'])
 async def create_post(post: models.PostBase, db: db_dependency):
-    db_post = models.Post(**post.dict())
+    db_post = models.Post(**post.model_dump())
     db.add(db_post)
     db.commit()
 
@@ -51,17 +62,14 @@ async def get_post(post_id: int, db: db_dependency):
         raise HTTPException(status_code=404, detail='Post was not found')
     return post
 
-@app.delete("/posts/{post_id", status_code=status.HTTP_200_OK)
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
 async def delete_post(post_id: int, db: db_dependency):
     db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if db_post is None:
         raise HTTPException(status_code=404, detail='Post was not found')
     db.delete(db_post)
     db.commit()
-
-@app.get('/', tags=['ROOT'])
-async def root() -> dict:
-    return dict(Ping='Pong', Hello='World')
 
 
 @app.get('/todo', tags=['todos'])
