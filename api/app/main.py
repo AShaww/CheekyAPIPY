5bfd7e6b7ce6d77277708aa504b22928b9d1d600
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-23 16:59:14 +0100

    clean up, added database, few new routes

---
+++ api/app/main.py
@@ -0,0 +1,114 @@
+import uvicorn
+from http.client import HTTPException
+from fastapi import FastAPI, Depends, status
+from typing import Annotated
+from api.app import models
+from api.app.db.database import engine, SessionLocal, Base
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED, tags=['users'])
+async def create_user(user: models.UserBase, db: db_dependency):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+
+
+@app.get('/users/{user_id}', status_code=status.HTTP_200_OK, tags=['users'])
+async def read_get(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(user_id == models.User.id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    return user
+
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED, tags=['posts'])
+async def create_post(post: models.PostBase, db: db_dependency):
+    db_post = models.Post(**post.dict())
+    db.add(db_post)
+    db.commit()
+
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK, tags=['posts'])
+async def get_post(post_id: int, db: db_dependency):
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code=404, detail='Post was not found')
+    return post
+
+
+@app.get('/', tags=['ROOT'])
+async def root() -> dict:
+    return dict(Ping='Pong', Hello='World')
+
+
+@app.get('/todo', tags=['todos'])
+async def get_todo() -> dict:
+    return {'data': todos}
+
+
+todos = [
+    {
+        "id": "1",
+        "Activity": "Jogging for 2 hours at 7:00 AM."
+    },
+    {
+        "id": "2",
+        "Activity": "Writing 3 pages of my new book at 2:00 PM."
+    }
+]
+
+
+@app.post('/todo', tags=['todos'])
+async def add_todo(todo: dict) -> dict:
+    todos.append(todo)
+    return {
+        'data': 'A todo has been added !'
+    }
+
+
+@app.put('/todo/{id}', tags=['todos'])
+async def update_todo(todo_id: int, body: dict) -> dict:
+    for todo in todos:
+        if int((todo['id'])) == todo:
+            todo['Activity'] = body['Activity']
+            return {
+                'data': f"Todo with id {todo} has been updated"
+            }
+
+    return {
+        'data': f"Todo with this id number {todo_id} was not found !"
+    }
+
+
+@app.delete('/todo/{id}', tags=['todos'])
+async def delete_todo(todo_id: int) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todos.remove(todo)
+            return {
+                "data": f"todo with id {todo_id} has been deleted"
+            }
+
+        return {
+            'data': f"Todo with this id number {todo_id} was not found !"
+        }
+
+
+if __name__ == '__main__':
+    uvicorn.run('api.app.main:app', port=8000, reload=True)
