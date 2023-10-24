a8124b4028d7183cf14aea34dff2865667e0c71a
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-24 23:59:57 +0100

    removed extra 'users' in routes, update prefix because it makes sense

--- api/app/routers/user.py
+++ api/app/routers/user.py
@@ -1,23 +1,22 @@
 from fastapi import APIRouter, HTTPException
 from starlette import status
-
 from api.app import models as m
 from api.app.auth.auth import db_dependency, user_dependency
 
 router = APIRouter(
-    prefix="/users",
+    prefix="/user",
     tags=["user"]
 )
 
 
 @router.get('/me')
-async def user_index(user_auth: user_dependency):
+async def user_profile(user_auth: user_dependency):
     if user_auth is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return {"User": user_auth}
 
 
-@router.get('/users/{user_id}', status_code=status.HTTP_200_OK)
+@router.get('/{user_id}', status_code=status.HTTP_200_OK)
 async def read_get_user(user_auth: user_dependency, user_id: int, db: db_dependency):
     user = db.query(m.UserBase).filter(user_id == m.Users.id).first()
     if user_auth is None:
