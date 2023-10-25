f30fdce22893ac02238c937f949098b925ca8405
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-25 11:53:36 +0100

    Created DatabaseManager, to handle instance con to db

--- api/app/auth/auth.py
+++ api/app/auth/auth.py
@@ -50,6 +50,7 @@ async def create_user(db: db_dependency,
         username=create_user_request.username,
         hashed_password=bcrypt_context.hash(create_user_request.password),
     )
+    #  Leaving this print here for debugging when I do the frontend login
     print(create_user_model.id, create_user_model.id, create_user_model.hashed_password)
     db.add(create_user_model)
     db.commit()
